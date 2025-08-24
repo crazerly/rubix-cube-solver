bb2607447fa4124b9da786c361f91fadef6b4ce4
crazerly <zachevans2692@gmail.com>
2025-08-24 15:06:42 +1000

    Initial Commit

---
+++ scan.js
@@ -0,0 +1,37 @@
+const camVideo = document.getElementById("camVideo");
+const scanBtn = document.getElementById('scanCube');
+const capturedImg = document.getElementById('capturedImage');
+
+async function startCamera() {
+    const constraints = {
+    video: {
+        width: 1280,
+        height: 720,
+        facingMode: "environment",
+    },
+    };
+    navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((stream) => {
+        camVideo.srcObject = stream;
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+function captureImage() {
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  
+  canvas.width = camVideo.videoWidth;
+  canvas.height = camVideo.videoHeight;
+  
+  context.drawImage(camVideo, 0, 0, canvas.width, canvas.height);
+  const imageData = canvas.toDataURL('image/png');
+  capturedImg.src = imageData;
+}
+
+startCamera();
+
+scanBtn.addEventListener('click', captureImage);
